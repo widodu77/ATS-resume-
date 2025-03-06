b57d48ee43dd38e789116ff6e069ff1f3ab3b27b
walid <walidajbar05@gmail.com>
2025-03-06 20:43:25 +0100

    ats front

---
+++ ats-front/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ResumeUpload from './ResumeUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeUpload />
+    </div>
+  );
+}
+
+export default App;
+
